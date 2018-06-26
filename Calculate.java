107e02058a858619e28f7a950d0cf015aedb9f8a
two4me <niboos2000@yandex.ru>
2018-06-26 11:46:34 +0530

    modificate Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,8 @@
 class Calculate {
 	public static void main(String[] args){
-		System.out.println("Hello from me");
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[1]);
+		int summ = first + second;
+		System.out.println("Hello from me, sum = " + summ);
 	}
 }
